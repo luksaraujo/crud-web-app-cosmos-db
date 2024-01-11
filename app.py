ec5518dd9913829291423169ab6c42befa3a9379
Lucas Araújo <lucasarasouza@hotmail.com>
2024-01-11 12:46:54 -0300

    Atualizando o app

--- app.py
+++ app.py
@@ -1,47 +1,65 @@
-import streamlit as st
-import controllers.crud as crud
-import pages.cliente.create as create_page
+import os, uuid
+from azure.cosmos import CosmosClient, exceptions
+from flask import Flask, render_template, request, redirect, url_for, flash
 
-st.sidebar.title('Menu')
-screen = st.sidebar.selectbox('Selecione uma tela para visualizar', ['Create', 'Read'])
-if screen == "Read":
-    params = st.experimental_get_query_params()
-    if params == {}:
-        st.experimental_set_query_params()
-        columns = st.columns((3, 2, 3, 1, 2, 2))
-        campos = ['UUID', 'Nome', 'CNPJ', 'Tipo', 'Excluir', 'Alterar']
-        for col, campo_nome in zip(columns, campos):
-            col.write(campo_nome)
-        for item in crud.read():
-            col1, col2, col3, col4, col5, col6 = st.columns((3, 2, 3, 1, 2, 2))
-            col1.write(item.uuid)
-            col2.write(item.nome)
-            col3.write(item.cnpj)
-            col4.write(item.tipo)
-            button_space_excluir = col5.empty()
-            on_click_excluir = button_space_excluir.button('Excluir', 'btnExcluir' + str(item.uuid))
-            button_space_alterar = col6.empty()
-            on_click_alterar = button_space_alterar.button('Alterar', 'btnAlterar' + str(item.uuid))
-            if on_click_excluir:
-                resposta = crud.delete(item.uuid, item.cnpj)
-                if resposta == '200':
-                    st.success('Estabelecimento excluído com sucesso!')
-                elif resposta == '404':
-                    st.error('Estabelecimento não encontrado!')
-                else:
-                    st.error('Erro ao excluir estabelecimento!\nErro: ' + resposta)
-                button_space_excluir.button('Excluído', 'btnExcluir' + str(item.uuid))
-            if on_click_alterar:
-                st.experimental_set_query_params(id=[item.uuid], cnpj=[item.cnpj])
-                st.experimental_rerun()
-    else:
-        on_click_voltar = st.button('Voltar')
-        if on_click_voltar:
-            st.experimental_set_query_params()
-            st.experimental_rerun()
-        create_page.create()
-elif screen == "Create":
-    st.experimental_set_query_params()
-    create_page.create()
-else:
-    pass
+app = Flask(__name__)
+app.secret_key = os.environ['FLASK_SECRET_KEY']
+
+cosmos_uri = os.environ['COSMOS_DB_URI']
+cosmos_key = os.environ['COSMOS_DB_KEY']
+cosmos_database = os.environ['COSMOS_DB_DATABASE']
+cosmos_container = os.environ['COSMOS_DB_CONTAINER']
+
+cosmos_client = CosmosClient(cosmos_uri, credential=cosmos_key)
+database_client = cosmos_client.get_database_client(cosmos_database)
+container_client = database_client.get_container_client(cosmos_container)
+
+@app.route('/')
+def index():
+    item_iterator = container_client.read_all_items()
+    storeList = [item for item in item_iterator]
+    return render_template('index.html', stores = storeList)
+
+@app.route('/create', methods=['POST'])
+def create():
+
+    def generate_uuid(partition_key):
+        while True:
+            random_uuid = str(uuid.uuid4())
+            try:
+                item = container_client.read_item(item=random_uuid, partition_key=partition_key)
+            except exceptions.CosmosResourceNotFoundError:
+                return random_uuid
+
+    if request.method == 'POST':
+        name = request.form['name']
+        cnpj = request.form['cnpj']
+
+        container_client.create_item({'name': name, 'cnpj': cnpj, 'id': generate_uuid(cnpj)})
+        
+        flash('Empresa cadastrada com sucesso!')
+        return redirect(url_for('index'))
+
+@app.route('/update', methods=['POST', 'GET'])
+def update():
+    if request.method == 'POST':
+        id_data = request.form['id']
+        name = request.form['name']
+        cnpj = request.form['cnpj']
+        
+        item = container_client.read_item(item=id_data, partition_key=cnpj)
+        item['name'] = name
+        item['cnpj'] = cnpj
+        container_client.replace_item(item=id_data, body=item)
+        flash('Empresa atualizada com sucesso!')
+        return redirect(url_for('index'))
+
+@app.route('/delete/<string:id_data>/<string:cnpj>', methods=['POST', 'GET'])
+def delete(id_data, cnpj):
+    container_client.delete_item(item=id_data, partition_key=cnpj)
+    flash('Empresa removida com sucesso!')
+    return redirect(url_for('index'))
+
+
+if __name__ == '__main__':
+    app.run(debug=False)
--- models/Store.py
+++
@@ -1,6 +0,0 @@
-class Store:
-    def __init__(self, uuid, nome, cnpj, tipo):
-        self.uuid = uuid
-        self.nome = nome
-        self.cnpj = cnpj
-        self.tipo = tipo
